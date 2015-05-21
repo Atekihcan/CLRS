676886e0de1af5728b63689ab0422ec98f8a6b45
Atekihcan <com.atekihcan@gmail.com>
2015-05-21 17:49:17 +0530

    adding code to capitalize first letter of each word of a span

--- assets/js/site.js
+++ assets/js/site.js
@@ -6,8 +6,20 @@ $(document).ready(function() {
         ($(".show-hide-button", this).text().slice(0, 1) == "\u25B6") ? $(".show-hide-button", this).text("\u25BC " + text_label.slice(1, text_len)) : $(".show-hide-button", this).text("\u25B6 " + text_label.slice(1, text_len));
     });
 
+    // Capitalize first letter of each word
+    capitalizeFirst('.cflew');
+
     // Initialize the plugin
     $('#popupBox').popup({
         transition: 'all 0.3s'
     });
 });
+
+function capitalizeFirst(elem) {
+    var words = $(elem).text().split(' ');
+    var html = '';
+    $.each(words, function() {
+        html += this.substring(0,1)+'<span style="font-size:80%">'+this.substring(1)+'</span> ';
+    });
+    $(elem).html(html);
+}

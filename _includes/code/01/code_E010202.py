98be5d507d2210f1e696b6ec57dbc7b7a5e4fa95
Atekihcan <Atekihcan@users.noreply.github.com>
2020-09-22 22:43:14 +0530

    Updating Chapter 1 | Close #10

--- _includes/code/01/code_E010202.py
+++ _includes/code/01/code_E010202.py
@@ -1,5 +1,5 @@
 n = 2
-while 2 ** (n / 8.0) < n:
+while n > 2 ** (n / 8.0):
     n += 1
 
-print("Maximum value of n for which insertion sort beats merge sort is", n - 1)
+print("Minimum value of n for which merge sort runs faster is", n)
